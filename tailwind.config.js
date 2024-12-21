01cb0e2889ca59f6affc7e031dce0d2a359cf4ed
Vivek Khatri <vivek.khatri@trialx.com>
2024-12-21 17:12:49 +0530

    Tryin what chatgpt says to deploy on vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
-import {nextui} from '@nextui-org/theme'
+// import {nextui} from '@nextui-org/theme'
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["var(--font-sans)"],
+//         mono: ["var(--font-geist-mono)"],
+//       },
+//     },
+//   },
+//   darkMode: "class",
+//   plugins: [nextui()],
+// }
+import { nextui } from '@nextui-org/theme';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,11 +30,11 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-geist-mono)"],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-geist-mono)'],
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
-}
+};
